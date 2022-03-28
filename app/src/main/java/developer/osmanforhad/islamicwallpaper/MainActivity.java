8a0a751706d12e2119de85cd82a4404b7d64141c
Osman Forhad <osmanforhad93@gmail.com>
2022-03-28 14:09:36 +0600

    fetch data from firebase database

--- app/src/main/java/developer/osmanforhad/islamicwallpaper/MainActivity.java
+++ app/src/main/java/developer/osmanforhad/islamicwallpaper/MainActivity.java
@@ -1,13 +1,19 @@
 package developer.osmanforhad.islamicwallpaper;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -24,12 +30,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //__Initial variable and connect with Database__//
+        reference = FirebaseDatabase.getInstance().getReference().child("images");
+
         //__Initial variables and connect with xml id__//
         recyclerView = findViewById(R.id.recyclerView);
         progressBar = findViewById(R.id.progressBar);
 
-        //__Initial variable and connect with Database__//
-        reference = FirebaseDatabase.getInstance().getReference().child("images");
+        getData();//Calling the method
 
     }
+
+    //__method for fetch data__//
+    private void getData() {
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                progressBar.setVisibility(View.GONE);
+                list = new ArrayList<>();
+                for(DataSnapshot shot: snapshot.getChildren()){
+                    String data = shot.getValue().toString();
+                    list.add(data);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                progressBar.setVisibility(View.GONE);
+                Toast.makeText(MainActivity.this, "Error: "+error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
 }
