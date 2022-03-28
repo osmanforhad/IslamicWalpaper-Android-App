59344b80307ce5e413859d7fb321f60b3dd42a6e
Osman Forhad <osmanforhad93@gmail.com>
2022-03-28 13:46:46 +0600

    setup direbase database connection and connect with xml view id from main activity

--- app/src/main/java/developer/osmanforhad/islamicwallpaper/MainActivity.java
+++ app/src/main/java/developer/osmanforhad/islamicwallpaper/MainActivity.java
@@ -1,14 +1,35 @@
 package developer.osmanforhad.islamicwallpaper;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.ProgressBar;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private ProgressBar progressBar;
+    private DatabaseReference reference;
+    private ArrayList<String> list;
+    private  WallpaperAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //__Initial variables and connect with xml id__//
+        recyclerView = findViewById(R.id.recyclerView);
+        progressBar = findViewById(R.id.progressBar);
+
+        //__Initial variable and connect with Database__//
+        reference = FirebaseDatabase.getInstance().getReference().child("images");
+
     }
 }
